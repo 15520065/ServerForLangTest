dd97cc4a0644310bf93fdaf862064c2a620f6542
15520065 <15520065@gm.uit.edu.vn>
2019-06-06 09:07:08 +0700

    version

--- src/App.js
+++ src/App.js
@@ -1,45 +1,35 @@
 import React from 'react';
-import {inject, observer} from 'mobx-react';
 import {BrowserRouter, Redirect, Route, Switch, withRouter} from 'react-router-dom';
-
-import commonStore from './stores/commonStore';
 import BarcodeLoadingPage from "./layouts/BarcodeLoadingPage";
 
 import LoadingScreen from "./layouts/LoadingScreen";
+import GetData from "./layouts/GetData";
+import {observer} from "mobx-react";
 
-
-@inject('userStore', 'commonStore')
 @withRouter
 @observer
 class App extends React.Component {
     componentWillMount() {
-        if (!this.props.commonStore.token) {
-            this.props.commonStore.setAppLoaded();
-        }
     }
 
     componentDidMount() {
-        if (this.props.commonStore.token) {
-            this.props.userStore
-                .pullUser()
-                .finally(() => this.props.commonStore.setAppLoaded());
-        }
     }
 
     render() {
-        if (this.props.commonStore.appLoaded) {
-            return (
-                <BrowserRouter>
-                    <Switch>
-                        <Route
-                            path="/"
-                            render={props => <BarcodeLoadingPage {...props} />}
-                        />
-                    </Switch>
-                </BrowserRouter>
-            );
-        }
-        return <LoadingScreen/>;
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route
+                        path="/a"
+                        render={props => <GetData {...props} />}
+                    />
+                    <Route
+                        path="/"
+                        render={props => <BarcodeLoadingPage {...props} />}
+                    />
+                </Switch>
+            </BrowserRouter>
+        );
     }
 }
 
--- src/layouts/BarcodeLoadingPage.jsx
+++ src/layouts/BarcodeLoadingPage.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
-import {withRouter} from 'react-router-dom';
 // reactstrap components
-import {inject, observer} from "mobx-react";
-import {KbnValue} from "../constants/KbnValue";
 import LoadingScreen from "./LoadingScreen";
 
 class BarcodeLoadingPage extends React.Component {
